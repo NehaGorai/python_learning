27b355c6f218193cdc280387c578f019ee6403f6
Neha Gorai <nhgorai@gmail.com>
2024-08-02 00:06:25 +0530

    Day1

---
+++ aug1.py
@@ -0,0 +1,20 @@
+print("printing.....")
+print(100)
+print(20+20)
+print(100/2)
+# Add comments
+print("How I can add new line using escape sequance")
+print("Python is a high-level, general-purpose programming language. \nIts design philosophy emphasizes code readability with the use of significant indentation. \nPython is dynamically typed and garbage-collected. \nIt supports multiple programming paradigms, including structured, object-oriented and functional programming.")
+print("printing.....") #only comments
+
+'''multiline code comment with 3 quotes
+ Python is a high-level, general-purpose programming language.
+Its design philosophy emphasizes code readability with the use of significant indentation. 
+Python is dynamically typed and garbage-collected.
+It supports multiple programming paradigms, including structured, object-oriented and functional programming.'''
+
+# print("Python is a high-level, "general-purpose" programming language.")  #It will throe an error
+print("Python is a \"high-level\" general-purpose programming language.") 
+print("hello" , "Dear", "Students")
+print("hello" , "Dear", "Students", sep="-") #sperate by any seprator.
+print("hello" , "Dear", "Students", sep="-",end="yooooo\n") 
